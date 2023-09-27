ff7852cf05f425705d59bd7f4e013d2b7f13b4b3
varunguptha <varunguptha987@gmail.com>
2023-09-27 19:22:42 +0530

    ds

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(cors({
-    origin: [""],
+    origin: ["https://feeds-fronted.vercel.app/"],
     credentials: true
 }))
 
